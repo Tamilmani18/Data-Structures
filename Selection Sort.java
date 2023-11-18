55cd585b255164c9c3a585484c6fa338dff47b1b
Tamil <144092813+Tamilmani18@users.noreply.github.com>
2023-11-18 21:30:15 +0530

    Selection Sort

---
+++ Selection Sort.java
@@ -0,0 +1,36 @@
+public class B{
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter no of elements :");
+		int n=sc.nextInt();
+		
+		int[] arr= new int[n];
+		System.out.print("Enter array elements :");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+		// swap until before last element
+		for(int i=0;i<=n-2;i++) { 
+			// Assume the current index is the minimum element
+			int min=i;
+			// compare each Element with Minimum Element
+			// find the Minimum element
+			for(int j=i;j<=n-1;j++) { // 0 to i is sorted
+				if(arr[j]<arr[min]) {
+					min=j;
+				}
+			}
+			//Swap the element
+			int temp=arr[min];
+			arr[min]=arr[i];
+			arr[i]=temp;
+		}
+    // print the sorted array
+		for(int i=0;i<n;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
